b1e1a2c6134a0d4364fe4e281d7a8e1a18b8e6d4
mohammad badri <mohammad.b@arianalabs.com>
2022-05-04 10:57:19 +0430

    delete extra files

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,5 @@
-import Link from "next/link";
-
 const Home = () => {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/articles">
-            <a>Article</a>
-          </Link>
-        </li>
-      </ul>
-      {/* Article Routes  */}
-      <h2>Article</h2>
-      <ul>
-        <li>
-          <Link href="/articles?title=1&id=1">
-            <a>Article Main Page</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/articles/[title]" as="/articles/article-1">
-            <a>Article Handle Dynamic title</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/articles/[title]/[id]" as="/articles/article-1/3">
-            <a>Article3</a>
-          </Link>
-        </li>
-      </ul>
-    </>
-  );
+  return <>Hello world</>;
 };
 
 export default Home;
