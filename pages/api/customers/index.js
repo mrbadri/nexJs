25146732f571249245247404dfd2e68fda243d49
mohammad badri <badri0000badri@gmail.com>
2022-05-09 23:30:02 +0430

    add CORS middleware

--- pages/api/customers/index.js
+++ pages/api/customers/index.js
@@ -1,4 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
+
+const cors = Cors({
+  origin: "http://localhost:3000",
+});
 
 let customers = [
   { id: 1, name: "customer 1" },
@@ -8,6 +13,18 @@ let customers = [
   { id: 5, name: "customer 5" },
 ];
 
-export default function handler(req, res) {
-  res.status(200).json({ data: customers, status: "success" });
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) return reject(result);
+      
+      return resolve(result);
+    });
+  });
 }
+
+export default async (req, res) => {
+  await runMiddleware(req, res, cors);
+
+  res.status(200).json({ data: customers, status: "success" });
+};
